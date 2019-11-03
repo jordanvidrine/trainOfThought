a84120c02b3dd85d3bec205708d3eff22e935877
Jordan Vidrine <jordan@jordanvidrine.com>
2019-11-02 23:32:37 -0500

    Added colors to grid, refactored code, stylizing, added buttons for debugging

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 var app = "train of thought";
 
-const GridSize = 25;
+const GridSize = 50;
 const SquareSize = 10;
+const Border = 1;
+const Dimension = SquareSize - (Border * 2)
+const Time = 10
+const Colors = [
+  'rgb(134, 158, 231)',
+  'rgb(115, 143, 230)',
+  'rgb(102, 132, 228)',
+  'rgb(89, 122, 228)',
+  'rgb(92, 125, 228)',
+  'rgb(75, 111, 227)',
+  'rgb(62, 101, 227)',
+  'rgb(45, 87, 224)'
+]
+
+var start = document.getElementById('start')
+var stop = document.getElementById('stop')
+
+start.addEventListener('click', () => {
+  buildGrid();
+  startThought();
+  start.setAttribute("disabled", 'true')
+})
+
+stop.addEventListener('click', () => {
+  clearInterval(state.clearIntervalId)
+})
+
 
 var state = {
   grid: [],
@@ -10,19 +37,26 @@ var state = {
   previousIndex: undefined,
 }
 
-// build the Array Grid based on the Grid Size ie. GridSize = 50, build an array of 50 arrays, each having 50 elements
-for (let i = 0; i < GridSize; i++) {
-  state.grid.push(new Array(GridSize).fill(0))
-}
-
-let table = document.createElement('table')
-
 function buildGrid() {
+
+  let table = document.createElement('table')
+
+  if (state.grid.length === 0) {
+    for (let i = 0; i < GridSize; i++) {
+      state.grid.push(new Array(GridSize).fill(0))
+    }
+  }
+
   for (let i = 0; i <= state.grid.length - 1; i++) {
     let row = document.createElement('tr')
     for (let j = 0; j <= state.grid[i].length - 1; j++) {
       let cell = document.createElement('td')
-      state.grid[i][j] === 1 ? cell.classList.add('on') : cell.classList.add('off')
+      let style = `width: ${Dimension}px; height: ${Dimension}px; border: ${Border}px solid grey;`
+
+      state.grid[i][j] === 1 ? style += `background-color:${Colors[Math.floor(Math.random()*Colors.length)]}`
+      : style += ''
+
+      cell.setAttribute('style', style)
       row.appendChild(cell)
     }
     table.appendChild(row)
@@ -36,11 +70,12 @@ function startThought() {
   state.grid[middle][middle] === 0 ? state.grid[middle][middle] = 1 : state.grid[middle][middle] = 0;
   state.currentIndex = {x:middle, y: middle}
 
+  let table = document.getElementsByTagName('table')[0]
   document.body.removeChild(table)
   table = document.createElement('table')
   buildGrid();
 
-  state.clearIntervalId = setInterval(continueThought,500)
+  state.clearIntervalId = setInterval(continueThought,Time)
 }
 
 function continueThought() {
@@ -58,6 +93,7 @@ function continueThought() {
 
   decision = choices[Math.floor(Math.random()*4)]
 
+  // make sure current index is inside the main grid
   if (decision.x >= 0 && decision.x <= GridSize - 1 && decision.y >= 0 && decision.y <= GridSize - 1) {
     state.currentIndex = decision;
 
@@ -65,6 +101,7 @@ function continueThought() {
     state.grid[state.currentIndex.x][state.currentIndex.y] = 1 :
     state.grid[state.currentIndex.x][state.currentIndex.y] = 0
 
+    let table = document.getElementsByTagName('table')[0]
     document.body.removeChild(table)
     table = document.createElement('table')
     buildGrid()
